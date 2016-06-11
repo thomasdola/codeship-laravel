38cbfe0fa76f673a21325b40832c21b3c82fdf0c
thomasdola <thomasdolar@gmail.com>
2016-06-11 00:44:53 +0000

    Added codeship env to project

---
+++ tests/LikesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class LikesTest extends TestCase
+{
+    /** @test */
+    public function a_user_can_like_a_post()
+    {
+
+    }
+}
