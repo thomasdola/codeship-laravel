cea3efb6b928dbbbc7f0b37c7744108d64b3857f
thomasdola <thomasdolar@gmail.com>
2016-06-11 00:49:24 +0000

    Added support for testing database

--- config/database.php
+++ config/database.php
@@ -52,6 +52,12 @@ return [
             'prefix' => '',
         ],
 
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('testing.sqlite')),
+            'prefix' => '',
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
